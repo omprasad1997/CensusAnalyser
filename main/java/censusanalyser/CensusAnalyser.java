c2568ebc8d022bf069893cffb84ac7bb4e6b95a6
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-11 16:12:44 +0530

    Refactored : Refactored code to close the file

--- main/java/censusanalyser/CensusAnalyser.java
+++ main/java/censusanalyser/CensusAnalyser.java
@@ -12,17 +12,16 @@ import java.util.stream.StreamSupport;
 
 public class CensusAnalyser {
     public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
-        try {
-            Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+        try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));){
             CsvToBeanBuilder<IndiaCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
             csvToBeanBuilder.withType(IndiaCensusCSV.class);
             csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
             CsvToBean<IndiaCensusCSV> csvToBean = csvToBeanBuilder.build();
             Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();
-            ;
             Iterable<IndiaCensusCSV> csvIterable = () -> censusCSVIterator;
             int numOfEntries = (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
             return numOfEntries;
+
         } catch (IOException e) {
             throw new CensusAnalyserException(e.getMessage(),
                               CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
@@ -30,5 +29,6 @@ public class CensusAnalyser {
             throw new CensusAnalyserException(e.getMessage(),
                                   CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
         }
+
     }
 }
